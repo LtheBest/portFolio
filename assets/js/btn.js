0a3c8f48e38bba4268e46aa822febd07ac240165
LtheBest <loicstonda@gmail.com>
2024-10-08 12:05:15 +0200

    update page bio

--- assets/js/btn.js
+++ assets/js/btn.js
@@ -6,9 +6,9 @@ document.querySelectorAll(".toggleButton").forEach(function (button) {
       moreContent.style.display === "none" || moreContent.style.display === "";
 
     if (isHidden) {
-      moreContent.style.display = "grid"; 
+      moreContent.style.display = "grid";
       this.firstChild.nodeValue = "Voir moins";
-      chevronIcon.className = "fas fa-chevron-up"; 
+      chevronIcon.className = "fas fa-chevron-up";
     } else {
       moreContent.style.display = "none";
       this.firstChild.nodeValue = "Voir plus ";
@@ -16,3 +16,18 @@ document.querySelectorAll(".toggleButton").forEach(function (button) {
     }
   });
 });
+
+function toggleText() {
+  var text = document.getElementById("moreText");
+  var button = document.getElementById("toggleButton");
+  var icon = document.getElementById("chevronIcon");
+  if (text.style.display === "none") {
+    text.style.display = "block"; 
+    button.innerHTML =
+      'Voir moins <i id="chevronIcon" class="fas fa-chevron-up"></i>'; 
+  } else {
+    text.style.display = "none"; 
+    button.innerHTML =
+      'Voir plus <i id="chevronIcon" class="fas fa-chevron-down"></i>'; 
+  }
+}
