0578b258d337847d164930a65b164ad61e2c3177
LtheBest <loicstonda@gmail.com>
2025-02-05 15:51:49 +0100

    mises ajour porteFolio

--- assets/js/dwonloads.js
+++ assets/js/dwonloads.js
@@ -1,19 +1,6 @@
 document.getElementById('downloadBtn').addEventListener('click', function() {
-    const cvs = [
-        "assets/cv/CV_Dev.pdf",
-        "assets/cv/CV_TECH.pdf",
-        "assets/cv/CV_DevOps.pdf",
-        "assets/cv/CV_TEST.pdf"
-    ];
-
-    // Sélectionne un CV au hasard
-    const randomCv = cvs[Math.floor(Math.random() * cvs.length)];
-
-    // Applique l'animation
     this.classList.add('downloading');
-
     setTimeout(() => {
-        // Redirige vers le CV sélectionné
-        window.location.href = randomCv;
+        window.location.href = this.getAttribute('href');
     }, 1000); 
 });
